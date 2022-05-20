cf026507db7c2235dfef13ba54bc8653a2ed0c9c
iosif-tihonenkov <iosif-tihonenkov@gmail.com>
2022-05-21 00:10:37 +0300

    add content

--- scripts/script.js
+++ scripts/script.js
@@ -4,30 +4,39 @@ function rewrite() {
     document.getElementById("button_one").style.width = "100px";
     document.getElementById("button_one").style.height = "100px";
     document.getElementById("button_one").textContent = '';
+    document.getElementById("colorBackgroundCode").textContent = '';
+    document.getElementById("colorTextCode").textContent = '';
+    document.getElementById("widthCode").textContent = '';
+    document.getElementById("heightCode").textContent = '';
+
 }
 
 function colorBackground() {
     let color = document.querySelector("input[name='colorBackground']").value;
     document.getElementById("button_one").style.backgroundColor = color;
     document.querySelector("input[name='colorBackground']").value = '';
+    document.getElementById("colorBackgroundCode").textContent = "background-color: " + color + ";";
 }
 
 function colorText() {
     let color = document.querySelector("input[name='colorText']").value;
     document.getElementById("button_one").style.color = color;
     document.querySelector("input[name='colorText']").value = '';
+    document.getElementById("colorTextCode").textContent = "color: " + color + ";";
 }
 
 function width() {
     let width = document.querySelector("input[name='width']").value;
     document.getElementById("button_one").style.width = width + 'px';
     document.querySelector("input[name='width']").value = '';
+    document.getElementById("widthCode").textContent = "     width: " + width + ";";
 }
 
 function height() {
     let height = document.querySelector("input[name='height']").value;
     document.getElementById("button_one").style.height = height + 'px';
     document.querySelector("input[name='height']").value = '';
+    document.getElementById("heightCode").textContent = "height: " + height + ";";
 }
 
 function content() {
