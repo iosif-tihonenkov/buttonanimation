124a842f9a768986e96edf83060be043a39d7a97
iosif-tihonenkov <iosif-tihonenkov@gmail.com>
2022-05-20 23:12:52 +0300

    add color text value

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,16 @@
-
-
 function colorBackground() {
-    let color123 = document.querySelector("input[name='colorBackground']").value;
-    document.getElementById("button_one").style.backgroundColor = color123;
+    let color = document.querySelector("input[name='colorBackground']").value;
+    document.getElementById("button_one").style.backgroundColor = color;
     document.querySelector("input[name='colorBackground']").value = '';
 }
 
 function rewrite() {
     document.getElementById("button_one").style.backgroundColor = '';
+    document.getElementById("button_one").style.color = '';
+}
+
+function colorText() {
+    let color = document.querySelector("input[name='colorText']").value;
+    document.getElementById("button_one").style.color = color;
+    document.querySelector("input[name='colorText']").value = '';
 }
